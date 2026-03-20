53b63ffe532d7b90429dbafb0bcededb56ac0b52
Aditya Bhumbla <adit.bhumbla@gmail.com>
2026-03-20 01:03:52 -0700

    More progress on raptor table

--- pkg/transit/raptor_table.go
+++ pkg/transit/raptor_table.go
@@ -4,7 +4,17 @@ import (
 	"fmt"
 	"router/pkg/types"
 	"sort"
+	"strings"
 )
+
+func Map[T any, U any](slice []T, f func(T) U) []U {
+	result := make([]U, len(slice))
+	for i, item := range slice {
+		result[i] = f(item)
+	}
+
+	return result
+}
 
 type Stop struct {
 	Name     string
@@ -51,11 +61,11 @@ func BuildRaptorTable(gtfsTable GtfsTable) (RaptorTable, error) {
 		activeGtfsTripIds[trip.GtfsID] = types.TripID(idx)
 	}
 
-	stopTimesByTrip := make(map[GtfsTripID][]GtfsStopTime)
+	stopTimesByTrip := make([][]GtfsStopTime, len(activeGtfsTripIds))
 
 	for _, st := range gtfsTable.StopTimes {
-		if _, ok := activeGtfsTripIds[st.GtfsTripID]; ok {
-			stopTimesByTrip[st.GtfsTripID] = append(stopTimesByTrip[st.GtfsTripID], st)
+		if tripID, ok := activeGtfsTripIds[st.GtfsTripID]; ok {
+			stopTimesByTrip[tripID] = append(stopTimesByTrip[tripID], st)
 		}
 	}
 
@@ -65,15 +75,60 @@ func BuildRaptorTable(gtfsTable GtfsTable) (RaptorTable, error) {
 		})
 	}
 
-	for _, trip := range gtfsTable.Trips[0:10] {
-		for _, st := range stopTimesByTrip[trip.GtfsID] {
-			fmt.Printf("trip %s seq %d stop %s arr %d dep %d\n",
-				st.GtfsTripID,
-				st.StopSequence,
-				st.GtfsStopID,
-				st.ArrivalTime,
-				st.DepartureTime,
-			)
+	type routeEntry struct {
+		stopSequence []types.StopID
+		trips        [][]GtfsStopTime
+	}
+
+	stopSequenceKey := func(stopSequence []types.StopID) string {
+		return strings.Join(Map(stopSequence, func(stopID types.StopID) string { return fmt.Sprintf("%d", stopID) }), ",")
+	}
+
+	routeMap := make(map[string]*routeEntry)
+
+	for _, stopTimes := range stopTimesByTrip {
+		stopSequence := make([]types.StopID, len(stopTimes))
+		for i, stopTime := range stopTimes {
+			stopID := gtfsStopIdToIdx[stopTime.GtfsStopID]
+			stopSequence[i] = stopID
+		}
+
+		tripKey := stopSequenceKey(stopSequence)
+
+		route, ok := routeMap[tripKey]
+		if !ok {
+			routeMap[tripKey] = &routeEntry{stopSequence, [][]GtfsStopTime{stopTimes}}
+		} else {
+			route.trips = append(route.trips, stopTimes)
+		}
+	}
+
+	routes := make([]routeEntry, 0, len(routeMap))
+	for _, route := range routeMap {
+		routes = append(routes, *route)
+	}
+
+	for _, route := range routes {
+		sort.Slice(route.trips, func(i, j int) bool {
+			return route.trips[i][0].DepartureTime < route.trips[j][0].DepartureTime
+		})
+	}
+
+	for routeID, route := range routes[0:5] {
+		fmt.Printf("route %d\n", routeID)
+
+		for tripNum, trip := range route.trips[0:min(len(route.trips), 2)] {
+			fmt.Printf("trip %d\n", tripNum)
+
+			for _, st := range trip {
+				fmt.Printf("trip %s seq %d stop %s arr %d dep %d\n",
+					st.GtfsTripID,
+					st.StopSequence,
+					st.GtfsStopID,
+					st.ArrivalTime,
+					st.DepartureTime,
+				)
+			}
 		}
 	}
 
