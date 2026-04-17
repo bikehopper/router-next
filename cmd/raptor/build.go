502802a1579920e1ea316cbff83d9936cb19ebb1
Andy McCoy <andy.k.mccoy@gmail.com>
2026-04-16 20:12:34 -0700

    Add CLI interface for app

--- cmd/raptor/build.go
+++ cmd/raptor/build.go
@@ -1,26 +1,53 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"time"
 
+	"router/pkg/debug"
 	"router/pkg/gtfs"
 	"router/pkg/raptor"
 )
 
 func main() {
-	gtfsTable, err := gtfs.ParseGtfs(os.Args[1])
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v", err)
+	if len(os.Args) < 2 {
+		printUsage()
 		os.Exit(1)
 	}
 
-	raptorTable, err := raptor.BuildRaptorTable(gtfsTable, gtfs.TimeToGTFSDate(time.Now()))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v", err)
+	switch os.Args[1] {
+	case "serve":
+		serverPort := flag.String("port", "3456", "port to serve on")
+		flag.Parse()
+		debug.StartServer(*serverPort)
+	case "build":
+		gtfsPath := flag.String("gtfs", "./gtfs.zip", "path to GTFS zip file")
+		flag.Parse()
+		gtfsTable, err := gtfs.ParseGtfs(*gtfsPath)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v", err)
+			os.Exit(1)
+		}
+
+		raptorTable, err := raptor.BuildRaptorTable(gtfsTable, gtfs.TimeToGTFSDate(time.Now()))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("Total size: %.2fMB\n", float64(raptorTable.Sizeof())/1024/1024)
+	default:
+		printUsage()
 		os.Exit(1)
 	}
 
-	fmt.Printf("Total size: %.2fMB\n", float64(raptorTable.Sizeof())/1024/1024)
+}
+
+func printUsage() {
+	fmt.Println("Usage: router-next <command> [flags]")
+	fmt.Println("\nCommands:")
+	fmt.Println("  serve      Start the debug web server")
+	fmt.Println("  build      Build RAPTOR table")
 }
