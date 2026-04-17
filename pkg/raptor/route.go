47955d3885300bf8993f6e84dd516f42f5bbeae6
Aditya Bhumbla <adit.bhumbla@gmail.com>
2026-04-16 22:26:11 -0700

    round-based pareto frontier

--- pkg/raptor/route.go
+++ pkg/raptor/route.go
@@ -13,25 +13,46 @@ type Label struct {
 	AlightStopIdx StopIndex
 }
 
+type Journey struct {
+	NumLegs     int
+	ArrivalTime types.Timestamp
+	Legs        []Label
+}
+
 const MAX_ROUNDS = 10
 
-func (rt *RaptorTable) Route(start types.StopID, end types.StopID, startTime types.Timestamp) []Label {
-	parent := make([]Label, rt.NumStops())
+func (rt *RaptorTable) Route(start types.StopID, end types.StopID, startTime types.Timestamp) []Journey {
+	numStops := rt.NumStops()
 
-	best := make([]types.Timestamp, rt.NumStops())
+	parents := make([][]Label, MAX_ROUNDS+1)
+
+	rounds := make([][]types.Timestamp, MAX_ROUNDS+1)
+	for k := range rounds {
+		parents[k] = make([]Label, numStops)
+
+		rounds[k] = make([]types.Timestamp, numStops)
+		for s := range rounds[k] {
+			rounds[k][s] = types.INFINITY
+		}
+	}
+
+	rounds[0][start] = startTime
+
+	best := make([]types.Timestamp, numStops)
 	for i := range best {
 		best[i] = types.INFINITY
 	}
 
 	best[start] = startTime
 
-	var prevBest []types.Timestamp
-
 	stopsUpdated := []types.StopID{start}
 
-	for len(stopsUpdated) > 0 {
-		prevBest = make([]types.Timestamp, rt.NumStops())
-		copy(prevBest, best)
+	for round := range MAX_ROUNDS {
+		if len(stopsUpdated) == 0 {
+			break
+		}
+
+		copy(rounds[round], rounds[round-1])
 
 		routeEarliestStop := make(map[types.RouteID]StopIndex)
 
@@ -56,21 +77,25 @@ func (rt *RaptorTable) Route(start types.StopID, end types.StopID, startTime typ
 				currStopId := stopsForRoute[currStopIdx]
 
 				if currentTripIdx >= 0 {
-					events := rt.StopEventsForTrip(routeId, uint32(currentTripIdx))
-					if events[currStopIdx].ArrivalTime < best[currStopId] {
-						best[currStopId] = events[currStopIdx].ArrivalTime
-						parent[currStopId] = Label{
+					arrivalTime := rt.StopEventsForTrip(routeId, uint32(currentTripIdx))[currStopIdx].ArrivalTime
+					if arrivalTime < rounds[round][currStopId] {
+						rounds[round][currStopId] = arrivalTime
+
+						parents[round][currStopId] = Label{
 							RouteId:       routeId,
 							TripIdx:       uint32(currentTripIdx),
 							BoardStopId:   boardStopId,
 							BoardStopIdx:  StopIndex(boardStopIdx),
 							AlightStopIdx: StopIndex(currStopIdx),
 						}
-						nextStopsUpdated = append(nextStopsUpdated, currStopId)
+						if arrivalTime < best[currStopId] {
+							best[currStopId] = arrivalTime
+							nextStopsUpdated = append(nextStopsUpdated, currStopId)
+						}
 					}
 				}
 
-				prevArrival := prevBest[currStopId]
+				prevArrival := rounds[round-1][currStopId]
 				if prevArrival == types.INFINITY {
 					continue
 				}
@@ -100,12 +125,34 @@ func (rt *RaptorTable) Route(start types.StopID, end types.StopID, startTime typ
 		return nil
 	}
 
+	var pareto []Journey
+
+	for round := range MAX_ROUNDS {
+		if round == 0 {
+			continue
+		}
+
+		if rounds[round][end] > rounds[round-1][end] {
+			pareto = append(pareto, Journey{
+				NumLegs:     round,
+				ArrivalTime: rounds[round][end],
+				Legs:        reconstructLegs(start, end, round, parents),
+			})
+		}
+	}
+
+	return pareto
+}
+
+func reconstructLegs(start, end types.StopID, round int, parents [][]Label) []Label {
 	var legs []Label
 
-	for stop := end; stop != start; {
-		label := parent[stop]
+	stop := end
+	for round > 0 && stop != start {
+		label := parents[round][stop]
 		legs = append(legs, label)
 		stop = label.BoardStopId
+		round--
 	}
 
 	slices.Reverse(legs)
